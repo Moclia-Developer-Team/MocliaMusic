a179e063f430ab8d46213ed1c1b338735df9998f
STASWIT <wubaiyuan@outlook.com>
2020-09-19 21:25:31 +0800

    更新版本号到1.0.0-3

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ int main()
 
 				if (plain == "*MusicHelp")
 				{
-					const string MocMuse_Ver = "1.0.0-2 Release ";
+					const string MocMuse_Ver = "1.0.0-3 Release ";
 					const string MocMuse_Info = "MocliaMusic by STASWIT Version"
 						+ MocMuse_Ver;
 					const string Platform = "for Mirai-Http";
