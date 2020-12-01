a407eae2c85b9f7fb19413169a8586c09931b6f6
STASWIT <wubaiyuan@outlook.com>
2020-12-01 13:58:26 +0800

    fix bugs

--- scripts/build_win86.ps1
+++ scripts/build_win86.ps1
@@ -1,4 +1,4 @@
 mkdir build_x86
 cd .\build_x86
-cmake -G "Visual Studio 16 2019" -A x86  -DCMAKE_CONFIGURATION_TYPES:STRING="Release" -DCMAKE_INSTALL_PREFIX:PATH="..\out\install\x86-Release" -DVCPKG_TARGET_TRIPLET=x86-windows-static  -DCMAKE_TOOLCHAIN_FILE="../tools/vcpkg/scripts/buildsystems/vcpkg.cmake" ".."
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_CONFIGURATION_TYPES:STRING="Release" -DCMAKE_INSTALL_PREFIX:PATH="..\out\install\x86-Release" -DVCPKG_TARGET_TRIPLET=x86-windows-static  -DCMAKE_TOOLCHAIN_FILE="../tools/vcpkg/scripts/buildsystems/vcpkg.cmake" ".."
 cmake --build . --config "Release" --target "MocliaMusic"
